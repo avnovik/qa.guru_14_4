520490c7eee7c4c79bf86edf758e964f8ee20cd5
Novikov Aleksei <3900676@gmail.com>
2022-08-18 19:23:25 +0300

    added modal close check

--- src/test/java/com/demoqa/StudentFormTest.java
+++ src/test/java/com/demoqa/StudentFormTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class StudentFormTest {
@@ -44,5 +45,6 @@ public class StudentFormTest {
                 text("Picture.pic"),
                 text("Wellington Square, 25"));
         $("#closeLargeModal").click();
+        $(".modal-open").shouldNotBe(visible);
     }
 }
