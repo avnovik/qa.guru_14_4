a2ee82665f409ea88b6cdfa8bf6193d78107ecbb
Novikov Aleksei <3900676@gmail.com>
2022-08-20 00:27:57 +0300

    added best practice selectors

--- src/test/java/com/demoqa/StudentFormTest.java
+++ src/test/java/com/demoqa/StudentFormTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class StudentFormTest {
@@ -21,21 +22,42 @@ public class StudentFormTest {
 
         open("/automation-practice-form");
         $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+
+        //close banners
         executeJavaScript("$('footer').remove()");
         executeJavaScript("$('#fixedban').remove()");
         $("#firstName").setValue("James");
         $("#lastName").setValue("Bond");
         $("#userEmail").setValue("jb@007.gb");
+
+        //gender
         $("[for=gender-radio-3]").click();
+//        $("#gender-radio-3").parent().click();
+//        $(by("for", "gender-radio-3")).click();
         $("#userNumber").setValue("0123456789");
+
+        //calendar
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").$("[value='11']").click();
-        $(".react-datepicker__year-select [value='1900']").click();
-        $(".react-datepicker__day--0" + "21").click();
+        $(".react-datepicker__month-select").selectOption("July");
+//        $(".react-datepicker__month-select").selectOptionByValue("6");
+//        $(".react-datepicker__month-select").$("[value='6']").click();
+        $(".react-datepicker__year-select").selectOption("2008");
+//        $(".react-datepicker__year-select [value='2008']").click();
+        $(".react-datepicker__day--021:not(.react-datepicker__day--outside-month)").click();
+//        $(".react-datepicker__day--0" + "21").click();
         $("#subjectsInput").setValue("A").pressTab();
         $("#subjectsInput").setValue("ar").pressTab();
-        $("[for='hobbies-checkbox-3']").click();
+
+        //hobbies
+//        $("[for='hobbies-checkbox-3']").click();
+        $("#hobbiesWrapper").$(byText("Music")).click();
+
+        //upload
         $("#uploadPicture").uploadFromClasspath("Picture/Picture.pic");
+//        $("#uploadPicture").uploadFile(new File("src/test/resources/Picture/Picture.pic"));
+//        File imgFile = new File("src/test/resources/Picture/Picture.pic");
+//        $("#uploadPicture").uploadFile(imgFile);
+
         $("#currentAddress").setValue("Wellington Square, 25");
         $("#react-select-3-input").setValue("ha").pressTab();
         $("#react-select-4-input").setValue("k").pressTab();
@@ -47,6 +69,10 @@ public class StudentFormTest {
                 text("0123456789"),
                 text("Picture.pic"),
                 text("Wellington Square, 25"));
+
+        //table check
+//        $("#table-responsive table").$(byText("Date of Birth"))
+//                .parent().shouldHave(text("21 July,2008"));
         $("#closeLargeModal").click();
         $(".modal-open").shouldNotBe(visible);
     }
